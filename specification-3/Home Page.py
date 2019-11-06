a199c065112d2a80160c8d54eb8b3b80d6e07945
George <georgehayfield96@gmail.com>
2019-11-06 11:48:48 +0000

    strange errors

--- specification-3/Home Page.py
+++ specification-3/Home Page.py
@@ -1,12 +1,17 @@
-from flask import Flask
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
 
+@app.route('/')
 def home():
-    return
+    return render_template("homepage.html")
+
+
+@app.route('/first')
+def first():
+    return render_template()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
