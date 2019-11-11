2f4e51a6d440c4248bba5356b2800489504b905c
b7080037 <i.koryciarz@newcastle.ac.uk>
2019-11-11 15:09:44 +0000

    char_freq_plot with the selection of 10 most common characters to be displayed on the graph. added info about the frequency counters to the freq.py file

--- specification-1/char_freq_plot.py
+++ specification-1/char_freq_plot.py
@@ -5,25 +5,25 @@ top_char_display = 10
 
 
 def plot_bar_graph(freq_char):
+
+    # create a dictionary to which the 10 most frequent characters in the text will be added
+    char_dict = {}
+    # use the most_common method from the collections to extract the first 10 characters from the freq_char counter
+    most_common_characters = freq_char.most_common(10)
+    for i in most_common_characters:
+        char_dict[i[0]] = i[1]
+
     fig, ax = plt.subplots()
 
-    for character, frequency in freq_char.most_common(10):
+    characters = list(char_dict.keys())
+    y_pos = np.arange(len(characters))
+    frequency = list(char_dict.values())
 
-        characters = list(character)
-        frequency = list(frequency)
-        print(characters)
-        print(frequency)
-    # most_freq_char = characters[0:11]
-        y_pos = np.arange(len(characters))
-
-
-   # most_freq_char_freq = frequency[0:11]
-
-        ax.barh(y_pos, frequency, align='center')
-        ax.set_yticks(y_pos)
-        ax.set_yticklabels(characters)
-        ax.invert_yaxis()  # labels read top to bottom
-        ax.set_xlabel('Frequency')
-        ax.set_title('Character Frequency')
+    ax.barh(y_pos, frequency, align='center')
+    ax.set_yticks(y_pos)
+    ax.set_yticklabels(characters)
+    ax.invert_yaxis()  # labels read top to bottom
+    ax.set_xlabel('Frequency')
+    ax.set_title('Character Frequency')
 
     plt.show()
--- specification-1/freq.py
+++ specification-1/freq.py
@@ -5,9 +5,7 @@ import csvwriter
 
 freq_counter = collections.Counter
 
-# file path of an example book
-book = '../resources/txt/test_file.txt'
-# input the file path of the file you want to analyse
+# file path of an example book to be analysed
 analysed_file = '../resources/txt/book.txt'
 
 
@@ -30,33 +28,34 @@ def count_words(analysed_file_read):
     # into a list of tuples
     output = {}
     words_freq_sorted = sorted(words_freq.items(), key=lambda kv: kv[1], reverse=True)
+    # turn the sorted list of tuples back to a dictionary to be used for csv writer later
     for i in words_freq_sorted:
         output[i[0]] = i[1]
-    return(output)
-    #return words_freq
+    return output
 
 
 # counting the instances of each of the characters in the file
 def count_characters(analysed_file_read):
     f = open(analysed_file, "r", encoding='utf8')
     analysed_file_read = f.read().lower().replace(' ', '').splitlines()
+
     temp_string = "".join(analysed_file_read)
-    #freq_counter_sorted = sorted(freq_counter(temp_string).items(), key = lambda kv: kv[1, reverse=True]))
     return freq_counter(temp_string)
 
 
 if __name__ == '__main__':
-    # file path of an example book
-   # book = '../resources/txt/test_file.txt'
-    # input the file path of the file you want to analyse
-    # analysed_file = '../resources/txt/book.txt'
 
     f = open(analysed_file, "r", encoding='utf8')
     analysed_file_read = f.read().lower().splitlines()
 
+    info_words = 'This is a list of all words in the file ordered from the most to the least frequent:'
     freq_words = count_words(analysed_file_read)
+    info_char = 'This is a counter containing a list of all the characters from the file ordered from the most to the least frequent:'
     freq_char = count_characters(analysed_file_read)
-    print(freq_words)
-    print(freq_char)
+
+    print(info_words, freq_words)
+    print(info_char, freq_char)
+
     csvwriter.csv_writer(freq_words)
+
     char_freq_plot.plot_bar_graph(freq_char)
