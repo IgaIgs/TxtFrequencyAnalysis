7393b96688cb67f4d55d39f2bf7382b813c55061
Oliver Myers <b9018701@newcastle.ac.uk>
2019-11-15 09:09:34 +0000

    added functionality for user interaction.

--- specification-4/web_scraping_olly/web_scraping_olly.py
+++ specification-4/web_scraping_olly/web_scraping_olly.py
@@ -10,7 +10,6 @@ headers.update({'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) 
 url = "https://www.rugbyworldcup.com/matches"
 req = requests.get(url, headers)
 soup = BeautifulSoup(req.content, 'lxml')
-
 # scores = soup.find('div', class_='fixtures__team fixtures__team--a fixtures__team--winner')
 
 def remove(list):
@@ -18,21 +17,40 @@ def remove(list):
     list = [re.sub(pattern, '', i) for i in list]
     return list
 
+def display():
+    print()
+
+def menu(choice):
+
+    switcher = {
+        1: scores(),
+        2: matchwins()
+
+    }
+    return switcher.get(choice, "nothing")
+
+def matchwins():
+    print('yes')
+
+def scores():
+    count = 0
+    team1_ar, team1_score_ar, team2_score_ar = [47]
+    while count < 48:
+        for i in range(47):
+            team1_ar[i] = str.format(all_results[1].text.split(' ')[0])
+            team1_score_ar[i] = str.format(((all_results[1].text.split(' ')[1])[:6]).strip('Win'))
+            team2_score_ar[i] = str.format((all_results[0].text.split(' ')[2]).strip('Win'))
+    display()
+
 all_results = soup.findAll('a', class_="fixtures__match-link--score")
-
-print(all_results[0].text.split(' '))
-print(all_results[1].text.split(' '))
-
-# gives the name of the first team
-team1 = str.format(all_results[1].text.split(' ')[0])
-
-# gives the name of the second team
-
-
-# gives the score of the first team
-team1_score = str.format(((all_results[1].text.split(' ')[1])[:6]).strip('Win'))
-
-# gives the score of the second team
-team2_score = str.format((all_results[0].text.split(' ')[2]).strip('Win'))
-
-
+quitter = 0
+while quitter < 1:
+    print('1. Get Rugby Results')
+    print('2. How many matches did a team win.')
+    print('3. Who won the world cup?')
+    print('4. Quit')
+    choice = input('Please enter the number of the option of your choosing:')
+    if choice == 4:
+        quitter = 1
+    else:
+        menu(choice)
